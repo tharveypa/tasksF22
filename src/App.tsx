734ca38d2b4575cfe27eae709e3affdbf8036e52
tharveypa <tharvey@udel.edu>
2022-09-01 11:48:11 -0400

    changed spelling

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automagically reload.
+                automatically reload.
             </p>
         </div>
     );
