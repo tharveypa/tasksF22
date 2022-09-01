f74a59ad509dc87bf663e1f0b57adc304b5f7126
tharveypa <tharvey@udel.edu>
2022-09-01 10:17:09 -0400

    changed to magic

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Terry Harvey: Edit <code>src/App.tsx</code> and save. This page
-                will automatically reload.
+                Edit <code>src/App.tsx</code> and save. This page will
+                automagically reload.
             </p>
         </div>
     );
